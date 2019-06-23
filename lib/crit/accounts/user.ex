c6beb8dbc5b9d97f849298f327125f541bdec55c
Brian Marick <marick@exampler.com>
2019-06-23 17:07:54 -0500

    Starting on authorization

--- lib/crit/accounts/user.ex
+++ lib/crit/accounts/user.ex
@@ -5,6 +5,7 @@ defmodule Crit.Accounts.User do
   schema "users" do
     field :email, :string
     field :name, :string
+    field :password, :string, virtual: true
     field :password_hash, :string
 
     timestamps()
@@ -13,7 +14,29 @@ defmodule Crit.Accounts.User do
   @doc false
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:name, :email, :password_hash])
-    |> validate_required([:name, :email, :password_hash])
+    |> cast(attrs, [:name, :email])
+    |> validate_required([:name, :email])
+    |> validate_length(:name, min: 2, max: 255, count: :codepoints)
+    |> validate_length(:email, min: 5, max: 255, count: :codepoints)
   end
+
+  def registration_changeset(user, attrs) do
+    user
+    |> changeset(attrs)
+    |> cast(attrs, [:password])
+    |> validate_length(:password, min: 6, max: 100)
+    |> put_password_hash()
+  end
+
+  defp put_password_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, Pbkdf2.hash_pwd_salt(pass))
+        
+      _ ->
+        changeset
+    end
+  end
+
+
 end
